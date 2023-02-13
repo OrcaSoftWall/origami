a361713fbd1366e44efda89c5ec45ddf5f0ccecf
Mariusz Ilijew <checkanica@gmail.com>
2023-02-13 20:08:29 +0100

    feat: pages guard

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // import Publications from './pages/publications';
 // import RegisterPage from './pages/register';
 // import LoginPage from './pages/login';
@@ -6,23 +6,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import ShareThoughtsPage from './pages/share-thoughts';
 // import ErrorPage from './pages/error';
 import { lazy, Suspense } from 'react';
+import { useContext } from 'react';
+import UserContext from './Context';
 
 const LazyPublications = lazy(() => import('./pages/publications'))
-const LazShareThoughtsPage = lazy(() => import('./pages/share-thoughts'))
+const LazyShareThoughtsPage = lazy(() => import('./pages/share-thoughts'))
 const LazyRegisterPage = lazy(() => import('./pages/register'))
 const LazyLoginPage = lazy(() => import('./pages/login'))
 const LazyProfilePage = lazy(() => import('./pages/profile'))
 const LazyErrorPage = lazy(() => import('./pages/error'))
+
 const LazyNavigation = () => {
+    const context = useContext(UserContext)
+    console.log(context)
+    const loggedIn = context.user && context.user.loggedIn
     return (
         <BrowserRouter>
             <Suspense fallback={<h1>Loading</h1>}>
                 <Routes>
                     <Route path="/" element={<LazyPublications />} />
-                    <Route path="/share" element={<LazShareThoughtsPage />} />
+                    <Route path="/share" element={loggedIn ? (<LazyShareThoughtsPage />) : (<Navigate replace to="/login" />)} />
                     <Route path="/register" element={<LazyRegisterPage />} />
-                    <Route path="/login" element={<LazyLoginPage />} />
-                    <Route path="/profile/:userid" element={<LazyProfilePage />} />
+                    <Route path="/login" element={!loggedIn ? (<LazyLoginPage />) : (<Navigate replace to="/" />)} />
+                    <Route path="/profile/:userid" element={loggedIn ? (<LazyProfilePage />) : (<Navigate replace to="/login" />)} />
                     <Route path="*" element={<LazyErrorPage />} />
                 </Routes>
             </Suspense>
