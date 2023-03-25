fc08c7affead192c4d1d1e535cf62df3c89175f5
Mariusz Ilijew <checkanica@gmail.com>
2023-03-25 18:46:48 +0100

    feat: add_edit-user_and_navigation_update

--- src/pages/edit-user/index.js
+++ src/pages/edit-user/index.js
@@ -3,24 +3,40 @@ import Title from '../../components/title';
 import SubmitButton from '../../components/button/submit-button';
 import PageLayout from '../../components/page-layout';
 import Input from '../../components/input';
-import { Component, useState, useContext } from "react";
+import { Component, useState, useContext, useCallback, useEffect, useParams } from "react";
 import { useNavigate, useHistory } from "react-router-dom";
 import authenticate from '../../utils/authenticate';
 import UserContext from '../../Context';
 
 const EditUserPage = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState(null)
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
   const [rePassword, setRePassword] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const context = useContext(UserContext)
   const navigate = useNavigate()
+  const params = useParams()
+
+  const getData = useCallback(async () => {
+    const id = params.userid
+    const responce = await fetch(`http://localhost:9999/api/user?id=${id}`)
+    if (!responce.ok) {
+      navigate("/error")               // this.props.history.push("/error")
+    }
+    const user = await responce.json()
+    setUsername(user.username)
+  },[params.userid, navigate])
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   const handleSubmit = async (event) => {
     event.preventDefault()
     //console.log(this.context)
 
-    if (username && password && rePassword && password === rePassword) {
+    if (username && oldPassword && newPassword && rePassword && newPassword === rePassword) {
       await authenticate('http://localhost:9999/api/user/register',
         { username, password },
         (user) => {
@@ -37,11 +53,12 @@ const EditUserPage = () => {
   return (
     <PageLayout>
       <form className={styles.container} onSubmit={handleSubmit} >
-        <Title title="Register" />
+        <Title title="Edit profile" />
         <Input type="text" value={username} onChange={(e) => { setUsername(e.target.value); setErrorMessage(null) }} label="Username" id="username" />
-        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} label="Password" id="password" />
+        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} label="Old Password" id="old-password" />
+        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} label="New Password" id="password" />
         <Input type="password" value={rePassword} onChange={(e) => setRePassword(e.target.value)} label="Re-Password" id="re-password" />
-        <SubmitButton title="Register" />
+        <SubmitButton title="Edit!" />
         {errorMessage ? <Title title={errorMessage} /> : null}
       </form>
     </PageLayout>
